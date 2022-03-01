42b0510e1cf6c2cf2da34ca129ac2df30393bc43
Tomáš Kalný <xkalny@stuba.sk>
2022-03-01 14:59:11 +0100

    Maly fix

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from call_logger import CallLogger
 def main():
     sipfullyproxy.call_log = open('call_log.txt', 'a')
     sipfullyproxy.my_log = CallLogger(sipfullyproxy.call_log)
-    print("IP address:", ipaddress)
-    server = socketserver.UDPServer((ipaddress, 5060), UDPHandler)
+    print("IP address:", sipfullyproxy.ipaddress)
+    server = socketserver.UDPServer((sipfullyproxy.ipaddress, 5060), UDPHandler)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
